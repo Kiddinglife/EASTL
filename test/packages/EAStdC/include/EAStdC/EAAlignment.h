b35537d7fe6b124b7aa730b59dfa796809974cd9
mengdi.zhang <mengdi.zhang@playtech.com>
2018-02-28 16:47:12 +1100

    made test/packages/EAStdC/* c-styled

--- test/packages/EAStdC/include/EAStdC/EAAlignment.h
+++ test/packages/EAStdC/include/EAStdC/EAAlignment.h
@@ -12,54 +12,19 @@
 #define EASTDC_API
 #define EASTDC_LOCAL
 
-namespace EA {
-namespace StdC {
-
-
-	/// IsAligned
+	/// EA_StdC_IsAligned
 	/// 
 	/// Tells if a given integer is aligned to a given power-of-2 boundary.
 	/// Returns true for an input x value of 0, regardless of the value of a.
-	/// The template <a> value must be >= 1.
+	/// a value must be >= 1.
 	/// Example:
-	///    IsAligned<int, 8>(64)  ->  true
-	///    IsAligned<int, 8>(67)  ->  false
+	///    EA_StdC_IsAligned(8,64)  ->  true
+	///    EA_StdC_IsAligned(8,67)  ->  false
 	///
 	/// To consider: wouldn't it be better if the template arguments were reversed?
 	///
-	template <typename T, int a>
-	inline bool IsAligned(T x)
-	{
-		return (x & (a - 1)) == 0;
-	}
-
-	template <typename T, int a>
-	inline bool IsAligned(T* p)
-	{
-		return ((uintptr_t)p & (a - 1)) == 0;
-	}
-
-
-	/// IsAligned
-	/// 
-	/// Tells if a given integer is aligned to a given power-of-2 boundary.
-	/// Returns true for an input x value of 0, regardless of the value of a.
-	/// The alignment value a must be >= 1.
-	/// Example:
-	///    IsAligned(64, 8)  ->  true
-	///    IsAligned(67, 8)  ->  false
-	///
-	template <typename T>
-	inline bool IsAligned(T x, size_t a)
-	{
-		return (x & (a - 1)) == 0;
-	}
-
-	template <typename T>
-	inline bool IsAligned(T* p, size_t a)
-	{
-		return ((uintptr_t)p & (a - 1)) == 0;
-	}
+    #define EA_StdC_IsAlignedInteger(x,a) (((x) & ((a) - 1)) == 0)
+    #define EA_StdC_IsAlignedPtr(p,a) (((uintptr_t)(p) & ((a) - 1)) == 0)
 
 
 	/// GetAlignment
@@ -81,19 +46,7 @@ namespace StdC {
 	///    GetAlignment(7)  ->  1
 	///    GetAlignment(8)  ->  8
 	///    GetAlignment(9)  ->  1
-	template <typename T>
-	inline size_t GetAlignment(T x)
-	{
-		return (size_t)((x ^ (x - 1)) >> 1) + 1;
-	}
-
-	template <typename T>
-	inline size_t GetAlignment(T* p)
-	{
-		return (size_t)(((uintptr_t)p ^ ((uintptr_t)p - 1)) >> 1) + 1;
-	}
-
-
-}} // namespace EA::StdC
+    #define EA_StdC_GetAlignmentInteger(x) ((size_t)(((x) ^ ((x) - 1)) >> 1) + 1)
+    #define EA_StdC_GetAlignmentPtr(p) ((size_t)((((uintptr_t)(p)) ^ (((uintptr_t)(p)) - 1)) >> 1) + 1)
 
 #endif  // EASTDC_EAALIGNMENT_H
