472919a7c5dfbe8465ab447b1cd7651bd0a37929
mengdi.zhang <mengdi.zhang@playtech.com>
2018-03-01 13:51:19 +1100

    made test/packages/EAStdC/EADataTime.h c-styled

--- test/packages/EAStdC/include/EAStdC/EADateTime.h
+++ test/packages/EAStdC/include/EAStdC/EADateTime.h
@@ -7,21 +7,15 @@
 #ifndef EASTDC_EADATETIME_H
 #define EASTDC_EADATETIME_H
 
-#include <chrono>
+//#include <chrono>
+//inline EASTDC_API uint64_t GetTime()
+//{
+//	using namespace std::chrono;
+//	nanoseconds ns = duration_cast<nanoseconds>(system_clock::now().time_since_epoch());
+//	return ns.count();
+//}
 
-#define EASTDC_API
-
-namespace EA {
-namespace StdC {
-
-inline EASTDC_API uint64_t GetTime()
-{
-	using namespace std::chrono;
-	nanoseconds ns = duration_cast<nanoseconds>(system_clock::now().time_since_epoch());
-	return ns.count();
-}
-
-}} // namespace EA::StdC
-
+//@TODO
+#define EA_StdC_GetTime()
 
 #endif  // EASTDC_EADATETIME_H
