aa3a268065404be9bccb5842010509792d562559
mengdi.zhang <mengdi.zhang@playtech.com>
2018-03-01 13:56:06 +1100

    made test/packages/EAStdC/EAMemory.h c-styled

--- test/packages/EAStdC/include/EAStdC/EAMemory.h
+++ test/packages/EAStdC/include/EAStdC/EAMemory.h
@@ -14,67 +14,16 @@
 
 #define EASTDC_API
 
-namespace EA {
-namespace StdC {
-
-EASTDC_API const void* Memcheck8 (const void* p, uint8_t  c, size_t byteCount);
-EASTDC_API const void* Memcheck16(const void* p, uint16_t c, size_t byteCount);
-EASTDC_API const void* Memcheck32(const void* p, uint32_t c, size_t byteCount);
-EASTDC_API const void* Memcheck64(const void* p, uint64_t c, size_t byteCount);
+EASTDC_API const void* EA_StdC_Memcheck8 (const void* p, uint8_t  c, size_t byteCount);
+EASTDC_API const void* EA_StdC_Memcheck16(const void* p, uint16_t c, size_t byteCount);
+EASTDC_API const void* EA_StdC_Memcheck32(const void* p, uint32_t c, size_t byteCount);
+EASTDC_API const void* EA_StdC_Memcheck64(const void* p, uint64_t c, size_t byteCount);
 
 
-EASTDC_API inline uint8_t* Memset8(void* pDest, uint8_t c, size_t uint8Count)
-{
-	return (uint8_t*)memset(pDest, c, uint8Count);
-}
-
-
-EASTDC_API inline uint16_t* Memset16(void* pDest, uint16_t c, size_t count)
-{
-	EA_ASSERT(((uintptr_t)pDest & 1) == 0);
-
-	uint16_t*             cur = (uint16_t*)pDest;
-	const uint16_t* const end = (uint16_t*)pDest + count;
-	while(cur < end)
-		*cur++ = c;
-
-	return (uint16_t*)pDest;
-}
-
-
-EASTDC_API inline uint32_t* Memset32(void* pDest, uint32_t c, size_t count)
-{
-	EA_ASSERT(((uintptr_t)pDest & 3) == 0);
-
-	uint32_t*             cur = (uint32_t*)pDest;
-	const uint32_t* const end = (uint32_t*)pDest + count;
-	while(cur < end)
-		*cur++ = c;
-
-	return (uint32_t*)pDest;
-}
-
-
-EASTDC_API inline uint64_t* Memset64(void* pDest, uint64_t c, size_t count)
-{
-	EA_ASSERT(((uintptr_t)pDest & 7) == 0);
-
-	uint64_t*             cur = (uint64_t*)pDest;
-	const uint64_t* const end = (uint64_t*)pDest + count;
-
-	while(cur < end)
-		*cur++ = c;
-
-	return (uint64_t*)pDest;
-}
-
-
-EASTDC_API inline int Memcmp(const void* p1, const void* p2, size_t n)
-{
-	return memcmp(p1, p2, n);
-}
-
-
-}} // namespace EA::StdC
+EASTDC_API inline uint8_t* EA_StdC_Memset8(void* pDest, uint8_t c, size_t uint8Count);
+EASTDC_API inline uint16_t* EA_StdC_Memset16(void* pDest, uint16_t c, size_t count);
+EASTDC_API inline uint32_t* EA_StdC_Memset32(void* pDest, uint32_t c, size_t count);
+EASTDC_API inline uint64_t* EA_StdC_Memset64(void* pDest, uint64_t c, size_t count);
+EASTDC_API inline int EA_StdC_Memcmp(const void* p1, const void* p2, size_t n);
 
 #endif  // EASTDC_EASPRINTF_H
--- test/packages/EAStdC/source/EAMemory.cpp
+++ test/packages/EAStdC/source/EAMemory.cpp
@@ -6,12 +6,8 @@
 
 #include <EABase/eabase.h>
 #include <EAStdC/EAMemory.h>
-
-
-namespace EA {
-namespace StdC {
 	
-EASTDC_API const void* Memcheck8(const void* p, uint8_t c, size_t byteCount)
+EASTDC_API const void* EA_StdC_Memcheck8(const void* p, uint8_t c, size_t byteCount)
 {
 	for(const uint8_t* p8 = (const uint8_t*)p; byteCount > 0; ++p8, --byteCount)
 	{
@@ -23,7 +19,7 @@ EASTDC_API const void* Memcheck8(const void* p, uint8_t c, size_t byteCount)
 }
 
 
-EASTDC_API const void* Memcheck16(const void* p, uint16_t c, size_t byteCount)
+EASTDC_API const void* EA_StdC_Memcheck16(const void* p, uint16_t c, size_t byteCount)
 {
 	union U16 {
 		uint16_t c16;
@@ -42,7 +38,7 @@ EASTDC_API const void* Memcheck16(const void* p, uint16_t c, size_t byteCount)
 }
 
 
-EASTDC_API const void* Memcheck32(const void* p, uint32_t c, size_t byteCount)
+EASTDC_API const void* EA_StdC_Memcheck32(const void* p, uint32_t c, size_t byteCount)
 {
 	union U32 {
 		uint32_t c32;
@@ -67,7 +63,7 @@ EASTDC_API const void* Memcheck32(const void* p, uint32_t c, size_t byteCount)
 }
 
 
-EASTDC_API const void* Memcheck64(const void* p, uint64_t c, size_t byteCount)
+EASTDC_API const void* EA_StdC_Memcheck64(const void* p, uint64_t c, size_t byteCount)
 {
 	union U64 {
 		uint64_t c64;
@@ -85,4 +81,47 @@ EASTDC_API const void* Memcheck64(const void* p, uint64_t c, size_t byteCount)
 	return NULL;
 }
 
-}}
+EASTDC_API inline uint8_t* EA_StdC_Memset8(void* pDest, uint8_t c, size_t uint8Count)
+{
+    return (uint8_t*)memset(pDest, c, uint8Count);
+}
+
+
+EASTDC_API inline uint16_t* EA_StdC_Memset16(void* pDest, uint16_t c, size_t count)
+{
+    EA_ASSERT(((uintptr_t)pDest & 1) == 0);
+
+    uint16_t*             cur = (uint16_t*)pDest;
+    const uint16_t* const end = (uint16_t*)pDest + count;
+    while(cur < end)
+        *cur++ = c;
+
+    return (uint16_t*)pDest;
+}
+
+
+EASTDC_API inline uint32_t* EA_StdC_Memset32(void* pDest, uint32_t c, size_t count)
+{
+    EA_ASSERT(((uintptr_t)pDest & 3) == 0);
+
+    uint32_t*             cur = (uint32_t*)pDest;
+    const uint32_t* const end = (uint32_t*)pDest + count;
+    while(cur < end)
+        *cur++ = c;
+
+    return (uint32_t*)pDest;
+}
+
+
+EASTDC_API inline uint64_t* EA_StdC_Memset64(void* pDest, uint64_t c, size_t count)
+{
+    EA_ASSERT(((uintptr_t)pDest & 7) == 0);
+
+    uint64_t*             cur = (uint64_t*)pDest;
+    const uint64_t* const end = (uint64_t*)pDest + count;
+
+    while(cur < end)
+        *cur++ = c;
+
+    return (uint64_t*)pDest;
+}
