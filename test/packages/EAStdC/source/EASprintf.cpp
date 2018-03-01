ce52deda8b5b861326d086669f9db43e7f35425a
mengdi.zhang <mengdi.zhang@playtech.com>
2018-03-01 14:01:38 +1100

    made test/packages/EAStdC/EASprintf.h c-styled

--- test/packages/EAStdC/source/EASprintf.cpp
+++ test/packages/EAStdC/source/EASprintf.cpp
@@ -7,23 +7,30 @@
 #include <EABase/eabase.h>
 #include <EAAssert/eaassert.h>
 #include <EAStdC/EASprintf.h>
-#include <cwchar>
+#include <wchar.h>
 
-namespace EA {
-namespace StdC {
-
-EASTDC_API int Vsnprintf(char8_t* EA_RESTRICT pDestination, size_t n, const char8_t* EA_RESTRICT pFormat, va_list arguments)
+EASTDC_API int EA_StdC_Vsnprintf(char8_t* EA_RESTRICT pDestination, size_t n, const char8_t* EA_RESTRICT pFormat, va_list arguments)
 	{ return vsnprintf(pDestination, n, pFormat, arguments); }
 
-EASTDC_API int Vsnprintf(char16_t* EA_RESTRICT pDestination, size_t n, const char16_t* EA_RESTRICT pFormat, va_list arguments)
+EASTDC_API int EA_StdC_Vsnprintf(char16_t* EA_RESTRICT pDestination, size_t n, const char16_t* EA_RESTRICT pFormat, va_list arguments)
     { return vswprintf((wchar_t*)pDestination, n, (wchar_t*)pFormat, arguments); }
 
-EASTDC_API int Vsnprintf(char32_t* EA_RESTRICT pDestination, size_t n, const char32_t* EA_RESTRICT pFormat, va_list arguments)
+EASTDC_API int EA_StdC_Vsnprintf(char32_t* EA_RESTRICT pDestination, size_t n, const char32_t* EA_RESTRICT pFormat, va_list arguments)
  	{ return vswprintf((wchar_t*)pDestination, n, (wchar_t*)pFormat, arguments); }
 
 #if defined(EA_WCHAR_UNIQUE) && EA_WCHAR_UNIQUE
-EASTDC_API int Vsnprintf(wchar_t* EA_RESTRICT pDestination, size_t n, const wchar_t* EA_RESTRICT pFormat, va_list arguments)
+EASTDC_API int EA_StdC_Vsnprintf(wchar_t* EA_RESTRICT pDestination, size_t n, const wchar_t* EA_RESTRICT pFormat, va_list arguments)
 	{ return vswprintf(pDestination, n, pFormat, arguments); }
 #endif
 
-}}
+EASTDC_API int EA_StdC_Sprintf(T* EA_RESTRICT pDestination, const T* EA_RESTRICT pFormat, ...)
+{
+    va_list arguments;
+    va_start(arguments, pFormat);
+
+    auto result = vsprintf(pDestination, pFormat, arguments);
+
+    va_end(arguments);
+
+    return result;
+}
