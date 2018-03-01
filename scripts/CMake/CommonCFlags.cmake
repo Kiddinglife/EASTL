b44b05745b68481ee7336b84827d3b3b9d490f64
mengdi.zhang <mengdi.zhang@playtech.com>
2018-03-01 16:58:58 +1100

    updated cmakedflags.cmake

--- scripts/CMake/CommonCFlags.cmake
+++ scripts/CMake/CommonCFlags.cmake
@@ -2,55 +2,13 @@
 #-------------------------------------------------------------------------------------------
 # Compiler Flags
 # see https://cmake.org/cmake/help/v3.1/prop_tgt/C_STANDARD.html cmake only supports c11 c99 c90
+# cmake will automatically use the highest stdc if complier supports
+# in eacompilertraits.h we habe defined macro that indicates us what stdc this compiler supports
+# so here just use -std=c11 simply
 #-------------------------------------------------------------------------------------------
-if(UNIX AND "${CMAKE_C_COMPILER_ID}" STREQUAL "Intel" )
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c11 -fasm-blocks" )
-endif()
-
-# NOT LESS == GREATER_OR_EQUAL; CMake doesn't support this out of the box.
-if(CMAKE_C_COMPILER_ID MATCHES "AppleClang")
-    if(NOT (CMAKE_C_COMPILER_VERSION VERSION_LESS "6.1"))
-        SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c11")
-    endif()
-    if(CMAKE_C_COMPILER_VERSION VERSION_LESS "6.1" AND (NOT (CMAKE_C_COMPILER_VERSION VERSION_LESS "4.3")))
-        SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c99")
-    endif()
-    # It seems Apple started changing version numbers after 3.1, going straight to 4.0 after 3.1.
-    if(CMAKE_C_COMPILER_VERSION VERSION_LESS "4.3" AND (NOT (CMAKE_C_COMPILER_VERSION VERSION_LESS "3.1")))
-        SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c89")
-    endif()
-    if(CMAKE_C_COMPILER_VERSION VERSION_LESS "3.1")
-        message(FATAL_ERROR "Building with a Apple clang version less than 3.1 is not supported.")
-    endif()
-elseif(CMAKE_C_COMPILER_ID MATCHES "Clang") # non-Apple clangs uses different versioning.
-    if(NOT (CMAKE_C_COMPILER_VERSION VERSION_LESS "3.5.0"))
-        SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c11")
-    endif()
-    if(CMAKE_C_COMPILER_VERSION VERSION_LESS "3.5.0" AND (NOT (CMAKE_C_COMPILER_VERSION VERSION_LESS "3.2")))
-        SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c99")
-    endif()
-    if(CMAKE_C_COMPILER_VERSION VERSION_LESS "3.2" AND (NOT (CMAKE_C_COMPILER_VERSION VERSION_LESS "3.0")))
-        SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c90")
-    endif()
-    if(CMAKE_C_COMPILER_VERSION VERSION_LESS "3.0")
-        message(FATAL_ERROR "Building with a clang version less than 3.0 is not supported.")
-    endif()
-elseif(CMAKE_C_COMPILER_ID MATCHES "GNU")
-    if(NOT (CMAKE_C_COMPILER_VERSION VERSION_LESS "5.2.0"))
-        SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c11")
-    endif()
-    if(CMAKE_C_COMPILER_VERSION VERSION_LESS "5.2.0" AND (NOT (CMAKE_C_COMPILER_VERSION VERSION_LESS "4.8.1")))
-        SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c99")
-    endif()
-    if(CMAKE_C_COMPILER_VERSION VERSION_LESS "4.8.1" AND (NOT (CMAKE_C_COMPILER_VERSION VERSION_LESS "4.7.3")))
-        SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c90")
-    endif()
-    if(CMAKE_C_COMPILER_VERSION VERSION_LESS "4.7.3")
-        message(FATAL_ERROR "Building with a gcc version less than 4.7.3 is not supported.")
-    endif()
-endif()
-
-
 if (CMAKE_C_COMPILER_ID MATCHES "Clang" OR CMAKE_C_COMPILER_ID MATCHES "GNU")
+    # make it same to msvc that uses _DEBUG
     set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -D_DEBUG")
 endif()
+
+SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c11")
